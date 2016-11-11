a764cf52ff41c1a20c298115d00ad0dda5a726c7
angel <angelAI.com>
2016-11-11 20:01:39 +0800

    test

---
+++ main.c
@@ -0,0 +1,19 @@
+/* ************************************************************************
+ *       Filename:  main.c
+ *    Description:  
+ *        Version:  1.0
+ *        Created:  2016年11月11日 20时01分01秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *         Author:  YOUR NAME (), 
+ *        Company:  
+ * ************************************************************************/
+
+#include<stdio.h>
+int main()
+{
+	printf("very good\n");
+	return 0;
+}
+
+
